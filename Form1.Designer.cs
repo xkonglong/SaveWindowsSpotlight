637fa37c40dc3d759e6ecd3bb9f27f635a60de99
小恐龙 <xkonglong@users.noreply.github.com>
2019-04-15 14:18:54 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.nudWidth = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnLock = new System.Windows.Forms.Button();
+            this.btnDesktop = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "请指定要保存的文件夹路径:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolTip1.SetToolTip(this.label1, "双击此标签打开锁屏文件存放路径.");
+            this.label1.DoubleClick += new System.EventHandler(this.Label1_DoubleClick);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(14, 33);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(263, 21);
+            this.txtPath.TabIndex = 9;
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(283, 32);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(61, 23);
+            this.btnPath.TabIndex = 10;
+            this.btnPath.Text = "浏览(&B)";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.BtnPath_Click);
+            // 
+            // nudWidth
+            // 
+            this.nudWidth.Location = new System.Drawing.Point(99, 75);
+            this.nudWidth.Maximum = new decimal(new int[] {
+            4096,
+            0,
+            0,
+            0});
+            this.nudWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudWidth.Name = "nudWidth";
+            this.nudWidth.Size = new System.Drawing.Size(84, 21);
+            this.nudWidth.TabIndex = 11;
+            this.nudWidth.Value = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "图片宽度≥";
+            this.toolTip1.SetToolTip(this.label2, "双击此文字设置图片宽度为主显示器屏幕宽度");
+            this.label2.DoubleClick += new System.EventHandler(this.Label2_DoubleClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(199, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "px";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(240, 149);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(104, 23);
+            this.btnStart.TabIndex = 14;
+            this.btnStart.Text = "开始保存(&S)";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // btnLock
+            // 
+            this.btnLock.Location = new System.Drawing.Point(15, 120);
+            this.btnLock.Name = "btnLock";
+            this.btnLock.Size = new System.Drawing.Size(108, 23);
+            this.btnLock.TabIndex = 15;
+            this.btnLock.Text = "打开锁屏设置";
+            this.btnLock.UseVisualStyleBackColor = true;
+            this.btnLock.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnDesktop
+            // 
+            this.btnDesktop.Location = new System.Drawing.Point(15, 149);
+            this.btnDesktop.Name = "btnDesktop";
+            this.btnDesktop.Size = new System.Drawing.Size(108, 23);
+            this.btnDesktop.TabIndex = 16;
+            this.btnDesktop.Text = "打开壁纸设置";
+            this.btnDesktop.UseVisualStyleBackColor = true;
+            this.btnDesktop.Click += new System.EventHandler(this.BtnDesktop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 194);
+            this.Controls.Add(this.btnDesktop);
+            this.Controls.Add(this.btnLock);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.nudWidth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "小恐龙锁屏壁纸保存工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.NumericUpDown nudWidth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnLock;
+        private System.Windows.Forms.Button btnDesktop;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e) //打开锁屏设置
+        {
+            try
+            { 
+                System.Diagnostics.Process.Start("ms-settings:lockscreen");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text);
+            }
+
+        }
+
+        private void BtnDesktop_Click(object sender, EventArgs e)//打开桌面设置
+        {
+            try
+            { System.Diagnostics.Process.Start("ms-settings:personalization-background");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text);
+            }
+        }
+
+
+        private void BtnPath_Click(object sender, EventArgs e)//指定路径
+        {
+            try
+            {
+                FolderBrowserDialog fb = new FolderBrowserDialog();
+
+                fb.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+                fb.ShowNewFolderButton = true;
+                fb.ShowDialog();
+                txtPath.Text = fb.SelectedPath;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text);
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path= Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            txtPath.Text = path;
+
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)//开始保存图片
+        {
+            
+            string orgPic = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) +
+                @"\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\";
+
+            if (!Directory.Exists(orgPic))
+            {
+                MessageBox.Show("锁屏图片存放文件夹不存在,请检查系统版本或升级本程序!",this.Text);
+                return;
+            }
+            if(!Directory.Exists(txtPath.Text))
+            {
+                try
+                {
+                    Directory.CreateDirectory(txtPath.Text);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, this.Text);
+                    return;
+                }
+                
+            }
+
+            string[] files = Directory.GetFiles(orgPic);
+            int i = 0;
+            foreach (string file in files)
+            {
+                int w = Image.FromFile(file).Width;
+                Console.WriteLine(Image.FromFile(file).Width.ToString());
+                
+
+                if (w >= (int)nudWidth.Value)
+                {
+                    //MessageBox.Show(w.ToString());
+                    
+                    
+                    FileInfo f = new FileInfo(file);
+                    string newFile=txtPath.Text+"\\"+f.Name+ ".jpg";
+                    File.Copy(file, newFile, true);
+                    i++;
+                }
+
+                
+            }
+            string msg = string.Format("已有{0}个文件被复制.", i);
+            MessageBox.Show(msg, this.Text,MessageBoxButtons.OK,MessageBoxIcon.Information);
+            try
+            {
+                System.Diagnostics.Process.Start(txtPath.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text);
+            }
+        }
+
+        private void Label1_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                string orgPic = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) +
+                        @"\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\";
+                System.Diagnostics.Process.Start(orgPic);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text);
+            }
+        }
+
+        private void Label2_DoubleClick(object sender, EventArgs e)
+        {
+            int w=Screen.PrimaryScreen.Bounds.Width;
+            nudWidth.Value = w;
+
+        }
+    }
+
+
+}
